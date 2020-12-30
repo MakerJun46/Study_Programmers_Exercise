db2ce671a6a6c98e3a157f2c6fc17fac54cf7691
MakerJun46 <guswns6711@naver.com>
2020-12-30 15:27:12 +0900

    Level1

---
+++ Level 1/Level1_2016년.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include<iostream>
+
+using namespace std;
+
+string solution(int a, int b) {
+    string answer = "";
+    string day[7] = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+    int month[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int date = 0;
+
+    for (int i = 0; i < a - 1; i++) {
+        date += month[i];
+    }
+    date += b - 1;
+    date %= 7;
+    answer = day[date];
+
+    return answer;
+}
+
+int main() {
+
+    cout << solution(5, 24);
+
+    return 0;
+}
