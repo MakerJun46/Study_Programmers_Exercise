f73f8377b4108ed54759ac3a5022907b614fc8f6
MakerJun46 <guswns6711@naver.com>
2020-12-31 13:44:19 +0900

    Level1

---
+++ Level 1/Level1_3진법 뒤집기.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> num;
+    int tmp;
+
+    while (n > 2) {
+        tmp = n % 3;
+        num.push_back(tmp);
+        n /= 3;
+    }
+    num.push_back(n);
+
+    for (int i = num.size() - 1; i >= 0; i--) {
+        answer += num[i] * pow(3, num.size() - 1 - i);
+    }
+
+    return answer;
+}
+
+
+int main() {
+    cout << solution(125);
+}
