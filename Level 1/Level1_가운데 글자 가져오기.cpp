ef802509660386d00cd0af0f76a9791b475916aa
MakerJun46 <guswns6711@naver.com>
2021-01-01 13:59:46 +0900

    Level1

---
+++ Level 1/Level1_가운데 글자 가져오기.cpp
@@ -0,0 +1,44 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    int index = 0;
+
+    if(s.length() % 2 == 1){
+        index = (s.length() - 1) / 2;
+        answer = s[index];
+    }
+    else {
+        index = s.length() / 2 - 1;
+        answer = s[index];
+        index++;
+        answer += s[index];
+    }
+
+
+    return answer;
+}
+
+/* Best answer
+#include <string>
+
+using namespace std;
+
+string solution(string s) {
+    return s.length()&1 ? s.substr(s.length()*0.5,1) : s.substr(s.length()*0.5-1,2);
+}  // s.length()&1 ==> s.length() 문자열 길이가 홀수라면 비트 2진법 마지막 수가 1이므로 비트 연산자
+   // &1 연산 시 1(참)이 되고, 짝수라면 2진법 마지막 수가 반드시 0이므로 0(거짓)이 된다.. 똑똑한 코드 
+*/
+
+
+int main() {
+
+    cout << &5;
+
+    return 0;
+}
---
+++ Level 1/Level1_같은 숫자는 싫어.cpp
@@ -0,0 +1,47 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    answer.push_back(arr[0]);
+    int answerIndexCount = 0;
+
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] != answer[answerIndexCount]) {
+            answer.push_back(arr[i]);
+            answerIndexCount++;
+        }
+    }
+
+    return answer;
+}
+
+/* bestAnswer
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+
+    arr.erase(unique(arr.begin(), arr.end()),arr.end()); 
+    // unique(*startpoint, endpoint)=> 시작 지점부터 끝 지점까지 중복 요소를 벡터 밖으로 밀어낸다.
+    // 작업 후 return은 밀어낸 요소들의 시작점이므로
+    // erase(unique(...), arr.end()), arr.end());는 밀어낸 값을 삭제하는 코드
+
+    vector<int> answer = arr;
+    return answer;
+}
+*/
+
+int main() {
+
+    for (auto i : solution({ 4,4,4,3,3 })) {
+        cout << i << " ";
+    }
+
+}
