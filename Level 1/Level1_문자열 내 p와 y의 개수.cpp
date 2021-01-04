dea4438884fea08b5428c5dbb65a8c6311407b10
MakerJun46 <guswns6711@naver.com>
2021-01-04 18:20:24 +0900

    Level1

---
+++ Level 1/Level1_문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int countP, countY;
+    countP = countY = 0;
+
+    for (auto i : s) {
+        if (i == 'p' || i == 'P')
+            countP++;
+        if (i == 'y' || i == 'Y')
+            countY++;
+    }
+    if (countP != countY)
+        return false;
+
+    return answer;
+}
+
+int main() {
+    solution("pPooooyY");
+}
---
+++ Level 1/Level1_문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,48 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+bool compareChar(string left, string right) {
+    if (left.at(N) != right.at(N))
+        return  left.at(N) < right.at(N);
+    else
+        return left < right;
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    N = n;
+    vector<string> answer = strings;
+    sort(answer.begin(), answer.end(), compareChar);
+    return answer;
+}
+
+/* Best answer
+int i;
+
+bool compare (string a, string b) {
+    return a[i] == b[i] ? a < b : a[i] < b[i];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    i = n;
+    sort (strings.begin(), strings.end(), compare);
+    return strings;
+}
+*/
+
+
+
+int main() {
+    
+    vector<string> answer = solution({ "sun", "abcd", "abce","bed", "cdx" ,"car"}, 2);
+
+    cout << "==============\n";
+    for(auto i : answer){
+        cout << i << endl;
+    }
+}
