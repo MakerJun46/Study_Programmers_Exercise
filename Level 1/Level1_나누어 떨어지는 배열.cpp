0fff100fac24841db30f818c34d36153a1f1eb9c
MakerJun46 <guswns6711@naver.com>
2021-01-04 14:19:00 +0900

    Level1

---
+++ Level 1/Level1_나누어 떨어지는 배열.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+
+    for (auto i : arr) {
+        if (i % divisor == 0) {
+            answer.push_back(i);
+        }
+    }
+    if (answer.empty()) return { -1 };
+    else
+        sort(answer.begin(), answer.end());
+
+    return answer;
+}
+
+int main() {
+    vector<int> answer = solution({ 2,36,1,3 }, 1);
+
+    for (auto i : answer) {
+        cout << i << " ";
+    }
+
+}
---
+++ Level 1/Level1_두 정수 사이의 합.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    long max = (a > b ? a : b);
+    long min = a < b ? a : b;
+
+    if (max == min) return max;
+
+    for (long i = min; i <= max; i++) {
+        answer += i;
+    }
+
+    return answer;
+}
+
+int main() {
+    cout << solution(5, 3);
+}
