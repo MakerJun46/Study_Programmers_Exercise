d2529590080636d3879366e7a63ae28d566fbdb9
MakerJun46 <guswns6711@naver.com>
2021-01-05 15:53:04 +0900

    Level1

---
+++ Level 1/Level1_문자열 내림차순으로 배치하기.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    vector<char> sTmp;
+
+    for (int i = 0; i < s.length(); i++) {
+        sTmp.push_back(s[i]);
+    }
+
+    sort(sTmp.begin(), sTmp.end(), greater<char>());
+
+    for (auto i : sTmp) {
+        answer += i;
+    }
+
+    return answer;
+}
+
+/* Best Answer
+ string solution(string s) {
+    sort (s.begin(), s.end(), greater<char>());
+    return s;
+}
+*/
+
+
+int main() {
+    
+    string s = "Zbcdefg";
+
+    cout << solution(s);
+
+    return 0;
+}
---
+++ Level 1/Level1_문자열 다루기 기본.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+
+    for (auto i : s) {
+        if (!(47 < (int)i && (int)i < 58)) 
+            return false;
+    }
+    if (s.length() == 4 || s.length() == 6)
+        return answer;
+    else
+        return false;
+}
+
+/* Best Answer
+* bool solution(string s) {
+    bool answer = true;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (!isdigit(s[i]))
+            answer = false;
+    }
+
+    return s.size() == 4 || s.size() == 6 ? answer : false;
+}
+
+*/
+
+int main() {
+    cout << solution("a234") << "\n";
+    cout << solution("1234");
+
+    
+
+}
