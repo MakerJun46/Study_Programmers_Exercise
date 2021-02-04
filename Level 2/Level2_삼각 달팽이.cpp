67b604c502bcd2f25a763f546905fcbaa1aa76c2
MakerJun46 <guswns6711@naver.com>
2021-02-04 16:37:00 +0900

    Level2

---
+++ Level 2/Level2_삼각 달팽이.cpp
@@ -0,0 +1,60 @@
+﻿#include <string>
+#include <vector>
+#include "library.h"
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    vector<vector<int>> stair(n + 1);
+    int count = 1;
+    int stairIndex = 0;
+    vector<int>::iterator itLeft;
+
+    for (int k = 1; k > 0; k++) {
+        if (n <= 0) {
+            break;
+        }
+        if (k % 3 == 1) {
+            for (int i = 0; i < n; i++) {
+                stairIndex++;
+                stair[stairIndex].insert(indexLeft[stairIndex], count);
+                indexLeft[stairIndex] ++;
+                count++;
+            }
+            n--;
+        }
+        else if (k % 3 == 2) {
+            for (int i = 0; i < n; i++) {
+                stair[stairIndex].insert(indexLeft[stairIndex]++,count);
+                count++;
+            }
+            n--;
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                stairIndex--;
+                stair[stairIndex].insert(indexRight[stairIndex]--, count);
+                count++;
+            }
+            n--;
+        }
+    }
+
+    for (auto i : stair) {
+        for (auto j : i) {
+            answer.push_back(j);
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+
+    for (auto i : solution(6)) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
