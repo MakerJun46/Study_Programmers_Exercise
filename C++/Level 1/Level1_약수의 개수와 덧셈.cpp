f10d2b2c96f900ba18333d86f302560608c3da8a
MakerJun46 <guswns6711@naver.com>
2021-11-11 22:17:11 +0900

    level

---
+++ C++/Level 1/Level1_약수의 개수와 덧셈.cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool is_factor_Even_number(int a)
+{
+    int count = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+        if (a % i == 0)
+        {
+            count++;
+        }
+    }
+
+    return !(count & 1);
+}
+
+
+int solution(int left, int right) {
+    int answer = 0;
+
+    for (int i = left; i <= right; i++)
+    {
+        answer += is_factor_Even_number(i) ? i : i * -1;
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+}
