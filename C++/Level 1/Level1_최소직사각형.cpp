bf2d4bcb3048a3662e57ff7cf2705c942574cd9a
MakerJun46 <guswns6711@naver.com>
2021-11-12 16:51:26 +0900

    level

---
+++ C++/Level 1/Level1_최소직사각형.cpp
@@ -0,0 +1,58 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+
+    vector<int> larger_sizes;
+    vector<int> smaller_sizes;
+
+    for (int i = 0; i < sizes.size(); i++)
+    {
+        sizes[i][0] > sizes[i][1]? larger_sizes.push_back(sizes[i][0]), smaller_sizes.push_back(sizes[i][1])
+            : (larger_sizes.push_back(sizes[i][1]), smaller_sizes.push_back(sizes[i][0]));
+
+        /*
+        if (sizes[i][0] >= sizes[i][1])
+        {
+            larger_sizes.push_back(sizes[i][0]);
+            smaller_sizes.push_back(sizes[i][1]);
+        }
+        else
+        {
+            larger_sizes.push_back(sizes[i][1]);
+            smaller_sizes.push_back(sizes[i][0]);
+        }
+        */
+    }
+
+    answer = *max_element(larger_sizes.begin(), larger_sizes.end()) * *max_element(smaller_sizes.begin(), smaller_sizes.end());
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution({ {60, 50}, {30, 70}, {60, 30}, {80, 40} });
+}
+
+/*  best answer
+int solution(vector<vector<int>> sizes)
+{
+    int answer=0;
+
+    int w=0, h=0;
+    for(int i=0; i<sizes.size(); i++)
+    {
+        w=max(w,min(sizes[i][0],sizes[i][1]));  // 작은 값 중에 가장 큰 값
+        h=max(h,max(sizes[i][0],sizes[i][1]));  // 큰 값 중에 가장 큰 값
+    }
+    answer=w*h;
+
+    return answer;
+}
+*/
