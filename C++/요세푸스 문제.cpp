31a02a9c2f373b8041adda16b3b77cc62ec7a3ad
MakerJun46 <guswns6711@naver.com>
2021-11-15 23:32:46 +0900

    baekjoon

---
+++ C++/요세푸스 문제.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+void main()
+{
+	int n, k;
+
+	cin >> n >> k;
+	
+	list<int> list_numbers;
+	list<int>::iterator it = list_numbers.begin();
+
+	for (int i = 1; i <= n; i++)
+	{
+		list_numbers.push_back(i);
+	}
+
+	while (!list_numbers.empty())
+	{
+
+	}
+
+}
