f5b660fbe77ae9d8024d6a98b366ff52e7fd8355
MakerJun46 <guswns6711@naver.com>
2021-11-20 23:16:53 +0900

    level2

---
+++ C++/Level 2/Level2_카펫.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+
+    
+
+
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> answer = solution(10, 2);
+    
+    for (auto i : answer)
+    {
+        cout << i << endl;
+    }
+
+}
