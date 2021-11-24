1059d08e7e4fba6e866d0f1c711e4dd948a70aff
MakerJun46 <guswns6711@naver.com>
2021-11-24 21:03:45 +0900

    20211124

---
+++ C++/Level 2/Level2_구명 보트.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+
+    sort(people.rbegin(), people.rend());
+
+    int index = people.size() - 1;
+
+    for (int i = 0; i < people.size(); i++)
+    {
+        if (index <= i)
+        {
+            if (index == i)
+                answer++;
+            break;
+        }
+        else if (people[i] + people[index] <= limit)
+        {
+            index--;
+        }
+        answer++;
+    }   
+    return answer;
+}
+
+void main()
+{
+    //cout << solution({ 70, 50, 80, 50, 50 }, 100) << endl;
+    //cout << solution({ 40, 50, 150, 160 }, 200) << endl;
+
+    cout << solution({ 100, 500, 500, 900, 950 }, 1000) << endl;
+
+}
---
+++ C++/Level 2/Level2_피보나치 수.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> Fibo;
+
+    Fibo.push_back(0);
+    Fibo.push_back(1);
+
+    for (int i = 2; i <= n; i++)
+    {
+        Fibo.push_back((Fibo[i - 1] + Fibo[i - 2]) % 1234567);
+    }
+
+    return *(Fibo.end() - 1) % 1234567;
+}
+
+void main()
+{
+    cout << solution(5) << endl;
+}
+
