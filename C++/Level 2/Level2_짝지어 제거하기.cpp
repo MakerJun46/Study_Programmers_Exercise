cecc60e8781c7ef7bdb771249975aa8363b31b4c
MakerJun46 <guswns6711@naver.com>
2021-11-30 23:18:05 +0900

    20211130

---
+++ C++/Level 2/Level2_짝지어 제거하기.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include<string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    /* // 효율성 0점
+    for (int i = 0; i < s.length() - 1; ++i)
+    {
+        if (s.empty())
+            break;
+
+        char tmp = s[i];
+
+        if (tmp == s[i + 1])
+        {
+            s.erase(i, 2);
+            if (i == 0)
+                i--;
+            else
+                i -= 2;
+        }
+    }
+    */
+    
+    stack<char> tmp;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (!tmp.empty() && tmp.top() == s[i])
+            tmp.pop();
+        else
+            tmp.push(s[i]);
+    }
+    
+    return (int)tmp.empty();
+}
+
+void main()
+{
+    cout << solution("baabaa");
+}
