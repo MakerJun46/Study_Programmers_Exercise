3fd734272120852a80fcf3eb2846325e97e32b18
MakerJun46 <guswns6711@naver.com>
2021-12-08 16:52:21 +0900

    20211208

---
+++ C++/Level 2/Level2_점프와 순간이동.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+
+    while (n != 0)
+    {
+        ans += n % 2;
+        n /= 2;
+    }
+
+    return ans;
+}
+
+void main()
+{
+    cout << solution(5) << endl;
+    cout << solution(6) << endl;
+}
