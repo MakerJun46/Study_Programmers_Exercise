e92463fdf09e7f676b83a3243cfe9994b45ea8cd
MakerJun46 <guswns6711@naver.com>
2021-12-09 18:58:33 +0900

    20211209

---
+++ C++/Level 2/Level2_올바른 괄호.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char> st;
+
+    for (char c : s)
+    {
+        if (c == '(')
+            st.push(c);
+        else if (c == ')')
+        {
+            if (st.empty())
+                return false;
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+
+int main()
+{
+    cout << solution("()()") << endl;
+    cout << solution(")()(") << endl;
+}
