0ff5799acf6adcfc4c8399e8c5e89b995fe7bc88
MakerJun46 <guswns6711@naver.com>
2022-03-14 23:59:59 +0900

    20220314

--- C++/Level 1/Level1_신고 결과 받기.cpp
+++ C++/Level 1/Level1_신고 결과 받기.cpp
@@ -1,62 +1,61 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 #include <map>
 #include <sstream>
+#include <algorithm>
+#include <set>
 #include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-    vector<int> answer(id_list.size(), 0);
+	vector<int> answer(id_list.size(), 0);
 
-    map<string, int> report_users;
-    vector<pair<string, string>> reported_log;
+	map<string, int> reportCount;
+	set<pair<string, string>> reported_log;
 
-    string tmp;
+	string tmp;
 
-    for (int i = 0; i < report.size(); i++)
-    {
-        vector<string> vec_tmp;
+	for (int i = 0; i < report.size(); i++)
+	{
+		vector<string> vec_tmp;
 
-        stringstream ss(report[i]);
-        while (getline(ss, tmp, ' '))
-        {
-            vec_tmp.push_back(tmp);
-        }
+		stringstream ss(report[i]);
+		while (getline(ss, tmp, ' '))
+		{
+			vec_tmp.push_back(tmp);
+		}
 
-        auto p = make_pair(vec_tmp[0], vec_tmp[1]);
+		auto p = make_pair(vec_tmp[0], vec_tmp[1]);
 
-        if (find(reported_log.begin(), reported_log.end(), p) == reported_log.end())
-        {
-            reported_log.push_back(p);
-            report_users[vec_tmp[1]]++;
-        }
-    }
+		if (reported_log.find(p) == reported_log.end())
+		{
+			reported_log.insert(p);
+			reportCount[vec_tmp[1]]++;
+		}
+	}
 
-    for (auto i : reported_log)
-    {
-        if (report_users[i.second] >= k)
-        {
-            vector<string>::iterator it = find(id_list.begin(), id_list.end(), i.first);
-            int index = it - id_list.begin();
+	for (auto it = reported_log.begin(); it != reported_log.end(); it++)
+	{
+		if (reportCount[it->second] >= k)
+		{
+			answer.at(find(id_list.begin(), id_list.end(), it->first) - id_list.begin())++;
+		}
+	}
 
-            answer[index]++;
-        }
-    }
-
-    return answer;
+	return answer;
 }
 
 int main()
 {
-    vector<int> answer;
+	//vector<int>answer = solution({ "con", "ryan" },
+	//	{ "ryan con", "ryan con", "ryan con", "ryan con" }, 3);
 
-    //nswer = solution({ "muzi", "frodo", "apeach", "neo" }, { "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" }, 2);
-    answer = solution({ "con", "ryan" }, { "ryan con", "ryan con", "ryan con", "ryan con" }, 3);
+	vector<int> answer2 = solution({ "muzi", "frodo", "apeach", "neo" },
+		{ "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" }, 2);
 
-
-    for (auto i : answer)
-    {
-        cout << i << endl;
-    }
+	for (auto i : answer2)
+	{
+		cout << i << endl;
+	}
 }
